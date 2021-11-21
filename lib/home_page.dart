8ebe73315b30c83b87441317960781791889490c
Somya_Sahu <somyasahu10@gmail.com>
2021-11-21 16:17:43 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  int days = 40;
+  String name = "Somya";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mrinal Roy TV"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,15 @@
-import 'package:flutter/material.dart'; // Importing material dart package
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart'; // Importing material dart package
 
 void main() {
-  // main function
   runApp(MyApp()); // runs the widgets of the app
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
