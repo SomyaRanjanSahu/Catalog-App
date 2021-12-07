c94f02ae21768240c3a40ac6fba2be21f0f18f06
Somya_Sahu <somyasahu10@gmail.com>
2021-12-08 03:55:52 +0530

    Day 15 Completed

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily,
+      fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: AppBarTheme(
         color: Colors.white,
         elevation: 0.0,
@@ -18,4 +18,8 @@ class MyTheme {
       appBarTheme: AppBarTheme(
         centerTitle: true,
       ));
+
+  // Colors
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
 }
