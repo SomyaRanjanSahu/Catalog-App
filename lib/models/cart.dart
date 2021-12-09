5545e4a1c0499613c166fbd80ad779be5d983bc8
Somya_Sahu <somyasahu10@gmail.com>
2021-12-10 02:28:02 +0530

    Day 21 completed

---
+++ lib/models/cart.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_catalog/models/catalog.dart';
+
+class CartModel {
+  // Catalog Field
+  late CatalogModel _catalog;
+
+  // Collection of Ids - store Ids of each item
+  final List<int> _itemsIds = [];
+
+  // Get catalog
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    assert(newCatalog != null);
+    _catalog = newCatalog;
+  }
+
+  // Get items in the cart
+  List<Item> get items => _itemsIds.map((id) => _catalog.getById(id)).toList();
+
+  // Get total price
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  // Add items
+
+  void add(Item item) {
+    _itemsIds.add(item.id);
+  }
+
+  // Remove items
+
+  void remove(Item item) {
+    _itemsIds.remove(item.id);
+  }
+}
--- lib/widgets/home_widgets/catalog_list.dart
+++ lib/widgets/home_widgets/catalog_list.dart
@@ -11,7 +11,7 @@ class CatalogList extends StatelessWidget {
       shrinkWrap: true,
       itemCount: CatalogModel.items.length,
       itemBuilder: (context, index) {
-        final catalog = CatalogModel.getByPosition(index);
+        final catalog = CatalogModel.items[index];
         return InkWell(
           onTap: () => Navigator.push(
               context,
